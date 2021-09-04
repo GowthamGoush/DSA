2cb769d23834f7d37c414cb693d706b403387ec4
Gowtham B <63305824+GowthamGoush@users.noreply.github.com>
2021-09-04 23:13:43 +0530

    Create MaximumProductOfTwoNumbers.java
    Finding the largest two elements in an array with single iteration.

---
+++ Sorting/MaximumProductOfTwoNumbers.java
@@ -0,0 +1,30 @@
+// 1464. Maximum Product of Two Elements in an Array
+// LeetCode
+// Easy
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        
+        int max1 = Math.max(nums[0], nums[1]), max2 = Math.min(nums[0], nums[1]);
+        
+        for(int i = 2; i < nums.length; i++){
+            
+            if(nums[i] > max1){
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(nums[i] < max1) {                
+                if(nums[i] > max2) {
+                    max2 = nums[i];
+                }
+            }
+            else {
+               max2 = nums[i];
+            }
+        }
+        
+        return (max1 - 1)*(max2 - 1);
+        
+    }
+}
