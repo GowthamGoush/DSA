f8d56d52737ddda8ebe833958b42e081ba82d804
Gowtham B <63305824+GowthamGoush@users.noreply.github.com>
2021-09-09 23:55:23 +0530

    Create ValidPalindrome.java
    Concept of replace method and String Buffer.

---
+++ Sorting/ValidPalindrome.java
@@ -0,0 +1,14 @@
+// 125. Valid Palindrome
+// LeetCode
+// Easy
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        
+        return actual.equals(rev);    
+    }
+}
